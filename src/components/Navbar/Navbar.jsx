63122a441c324dd58ccdfcb04b363248a6baaec0
@chandan195 <chandanmariya98@gmail.com>
2024-11-07 13:48:29 +0530

    working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../../assets/logo.png";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div  className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40" >
+      {/* upper navbar*/}
+      <div className="bg-primary/40 py-2" >
+        <div className="container flex justify-between items-center">
+          <div>
+          <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={Logo} alt="Logo" className="w-10" />
+              Shopsy
+            </a>
+          </div>
+          {/* search bar and order button */}
+          <div>
+            <div className="group">
+              <input
+                type="text"
+                placeholder="search"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px]
+                 transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 
+                 focus:outline-none focus:border-1 focus:border-primary
+                  dark:border-gray-500 dark:bg-gray-800 "
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ower navbar */}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
